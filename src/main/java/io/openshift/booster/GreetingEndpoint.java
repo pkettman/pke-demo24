fab2d8a46f13318201790ae8ab0aaece7dceb13c
pkettman <pkettman@redhat.com>
2018-05-22 11:19:49 +0000

    Good morning

--- src/main/java/io/openshift/booster/GreetingEndpoint.java
+++ src/main/java/io/openshift/booster/GreetingEndpoint.java
@@ -25,7 +25,7 @@ import javax.ws.rs.QueryParam;
 @Path("/")
 public class GreetingEndpoint {
 
-    private static final String template = "Hello , %s!";
+    private static final String template = "Good morning, %s!";
 
     @GET
     @Path("/greeting")
